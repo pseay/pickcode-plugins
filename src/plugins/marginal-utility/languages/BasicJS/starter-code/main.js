cacf9cc81202f23e0b5cf66c0b72bdd784b56244
Preston Seay <preston.seay@outlook.com>
2025-06-26 11:36:15 -0700

    updated starter code

--- src/plugins/marginal-utility/languages/BasicJS/starter-code/main.js
+++ src/plugins/marginal-utility/languages/BasicJS/starter-code/main.js
@@ -1,9 +1,4 @@
 function choose(mu_a, p_a, mu_b, p_b) {
     // Write your code in place of this line.
-    if (mu_a/p_a > mu_b/p_b) {
-        return "apple";
-    } else {
-        return "banana";
-    }
 }
 optimize(choose)
